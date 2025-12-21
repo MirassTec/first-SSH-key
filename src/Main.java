47b6ed2736ad65a945a2e11754da20548b2d6b73
Miras <miras.aastudy@gmail.com>
2025-12-21 12:20:13 +0500

    Assignment 1 RECTANGLE OOP

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(5, 3);
+        System.out.println(r);
+        System.out.println("Area: " + r.area());
+        System.out.println("Perimeter: " + r.perimeter());
+    }
+}
